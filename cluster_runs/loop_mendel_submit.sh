cbba2e80012b6fee831e7789367e1e7acac9f4d0
Giorgos <gvernardos@amnh.org>
2024-05-21 18:12:37 -0400

    Modified mendel submission loop.

--- cluster_runs/loop_mendel_submit.sh
+++ cluster_runs/loop_mendel_submit.sh
@@ -5,6 +5,7 @@ runs=($(awk -F"," '{print $1}' sample_to_fit.csv))
 for run in "${runs[@]}"; do
 #for (( i=0; i<${#runs[@]}; i++ )); do
     #run=${runs[$i]}
-    echo $run
-    sbatch mendel_submit.sh ${run} /home/gvernardos/combined_posterior/input_files/ /home/gvernardos/combined_posterior/output/
+    printf -v qid "%05d" $run
+    echo $qid
+    sbatch mendel_submit.sh ${qid} /home/gvernardos/combined_posterior/input_files/ /home/gvernardos/combined_posterior/output/
 done
